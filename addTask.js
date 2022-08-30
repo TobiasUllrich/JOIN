206edd3aac057370f0420bbc2ae5cfdb26c0b8e9
Eugen Oswald <oswaldeugen95@gmail.com>
2022-08-30 17:17:56 +0200

    Addtask corrections & user picker prepared

--- addTask.js
+++ addTask.js
@@ -1,4 +1,31 @@
+let usersTEST = ['.img/edip.png', '.img/Tobias.png', '.img/eugen.png', '.img/gast.png']
+let selectedUsersTEST = []
+
+
+function render() {
+    let avatarPicker = document.getElementById('avatarPicker');
+    avatarPicker.innerHTML = '';
+
+    for (let i = 0; i < usersTEST.length; i++) {
+        const avatar = usersTEST[i];
+        avatarPicker.innerHTML += `<img id="user-${i}" onclick="selectUser(${i}) src="${avatar}" class="avatar"`;
+    }
+}
+
+
+function selectUser(i) {
+    let user = document.getElementById('user-' + i);
+    user.classList.toggle('avatar-selected');
+    if (selectedUsersTEST.includes(usersTEST[i])) {
+        //Löschen
+        selectedUsersTEST = selectedUsersTEST.filter(a => a !=user[i]);
+    } else {
+        selectedUsersTEST.push(usersTEST[i]);
+    }
+}
+
 function submitAdvice(f) {
     alert('submitting');
     return false;
 }
+
