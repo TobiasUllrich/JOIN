e362899437bd17ea9bf3e6ae70b8621a1b56625a
EdipRubenBahcecioglu <edip-bahcecioglu@hotmail.de>
2022-08-30 03:19:28 +0200

    second solo task layout with style

--- board.js
+++ board.js
@@ -1,24 +1,44 @@
-function templateOfNewTask(){
+function templateOfNewTask() { // INDEX MUSS NOCH ÜBERGEBEN WERDEN
     return `
-    <div class="main-task> onclick="showTaskDetails()">
+    <div id="main-task" class="main-task border-left-red" onclick="showTaskDetails()">
         <div class="task-headline">
-            <div class="task-image"><img src=".img/todo.ong"></div>
+            <div><img class="task-image" src="./img/todo.png"></div>
             <div class="task-titel">
                 <h3>TASKTITLE</h3>
                 <span>TASKDATE</span>
             </div>
-            <div class="delete-btn"><img src="./img/delete.png"></div>
+            <div class="delete-btn"><img class="delete-img" src="./img/delete.png"></div>
         </div>
 
         <div id="task-details" class="task-details d-none">
-            <span>Description: TASKDESCRIPTION</span>
-            <span>Category: TASKCATEGORY</span>
-            <span>Urgency: TASKURGENCY</span>
+            <table>
+                <tr>
+                    <td><b>Description:</b></td>
+                    <td>TASKDESCRIPTION</td>
+                </tr>
+
+                <tr>
+                    <td><b>Category:</b></td>
+                    <td>TASKCATEGORY</td>
+                </tr>
+
+                <tr>
+                    <td><b>Urgency:</b></td>
+                    <td>TASKURGENCY</td>
+                </tr>
+
+                <tr>
+                    <td><b>Assigned to:</b></td>
+                    <td>USER</td>
+                </tr>
+            </table>
         </div>
     </div>
+</div>
     `
 }
 
-function showTaskDetails(){
-    document.getElementById('task-details').classList.remove('d-none'); // Hier muss der ID noch der Index übergeben werden
+function showTaskDetails() {
+    document.getElementById('main-task').classList.toggle('twohundert-height'); // Hier muss der ID noch der Index übergeben werden
+    document.getElementById('task-details').classList.toggle('d-none');
 }
