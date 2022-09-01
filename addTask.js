dbd9d84fda7184a0e5699a412af90e8cb5cf22bb
Eugen Oswald <oswaldeugen95@gmail.com>
2022-09-01 20:48:44 +0200

    update

--- addTask.js
+++ addTask.js
@@ -24,6 +24,26 @@ function selectUser(i) {
     };
 }
 
+
+// onsubmit test
 function myFunction(){
     console.log('Hello');
+    category();
+}
+
+// selcet test
+function urgency() {
+    let urgency = document.getElementById('urgency');
+    let index = urgency.options[urgency.selectedIndex];
+
+    console.log(urgency, index);
+}
+
+
+// selcet test
+function category() {
+    let category = document.getElementById('category');
+    let index = category.options[category.selectedIndex];
+
+    console.log(category, index);
 }
