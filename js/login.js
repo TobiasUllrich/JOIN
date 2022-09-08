3b2b4c7b765ee4c99bb16c4fe633cd488c38e1c9
Tobias Ullrich <ullrich.tobias@gmx.de>
2022-09-08 23:07:32 +0200

    HTML5 Validation rdy

--- js/login.js
+++ js/login.js
@@ -83,4 +83,13 @@ function tryToSendEmail(){
     console.log('Email should have been sent');
     console.log('Email is ', document.getElementById('forgotpassword-email').value);  
     showResetPasswordForm();  
-}    
+}
+
+// Password benötigt mindestens einen Großbuchstaben, Kleinbuchstaben und eine Zahl    
+function checkPassword(str)
+{ 
+  // at least one number, one lowercase and one uppercase letter
+  // at least six characters that are letters, numbers or the underscore
+  var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
+  return re.test(str);
+}
