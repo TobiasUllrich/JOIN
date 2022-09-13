93c619e1a7b6d6ba88364197685b2472fc474d04
Tobias Ullrich <ullrich.tobias@gmx.de>
2022-09-13 12:17:28 +0200

    add

--- js/script.js
+++ js/script.js
@@ -28,77 +28,84 @@ let tasksScript = [
     "id": 0,
     "title": "Lasst uns starten :)",
     "category": "Design",  // Design, Marketing, Sales, Backoffice, Media usw. gerne weitere Vorschläge bzw. Ergänzungen
+    "categorycolor": "#091931",
     "description": "BlaBlaBla",
     "dueDate": "30-10-2010",
     "priority": "Medium",       // Urgent, Medium, Low
     "status": "To do",     // To do, In progress, Awaiting feedback, Done
-    "assignedTo": [0,1,2],   // Index of users Array (ist eindeutig)
+    "assignedTo": ['ullrich.tobias@gmx.de','edip-bahcecioglu@hotmail.de','oswaldeugen95@gmail.com'],   // Email of users Array (ist eindeutig)
     "subTasks": ['Putzen','Spülen','Saubermachen'] 
   },
   {
     "id": 1,
     "title": "Lagebesprechung Preiserhöhungen",
     "category": "Sales",  // Design, Marketing, Sales, Backoffice, Media usw. gerne weitere Vorschläge bzw. Ergänzungen
+    "categorycolor": "#091931",
     "description": "Hier wird ein unnötiger Beschreibungstext stehen, der von dem User festgelegt wird",
     "dueDate": "30-10-2010",
     "priority": "Urgent",       // Urgent, Medium, Low
     "status": "To do",     // To do, In progress, Awaiting feedback, Done
-    "assignedTo": [0,1,2],   // Index of users Array (ist eindeutig)
+    "assignedTo": ['ullrich.tobias@gmx.de','edip-bahcecioglu@hotmail.de','oswaldeugen95@gmail.com'],   // Email of users Array (ist eindeutig)
     "subTasks": ['Putzen','Spülen','Saubermachen'] 
   },
   {
     "id": 2,
     "title": "Planung der Weihnachtsfeier",
     "category": "Backoffice",  // Design, Marketing, Sales, Backoffice, Media usw. gerne weitere Vorschläge bzw. Ergänzungen
+    "categorycolor": "#091931",
     "description": "Hier wird ein unnötiger Beschreibungstext stehen, der von dem User festgelegt wird",
     "dueDate": "30-10-2010",
     "priority": "Low",       // Urgent, Medium, Low
     "status": "Awaiting feedback",     // To do, In progress, Awaiting feedback, Done
-    "assignedTo": [0,1,2],   // Index of users Array (ist eindeutig)
+    "assignedTo": ['ullrich.tobias@gmx.de','edip-bahcecioglu@hotmail.de','oswaldeugen95@gmail.com'],   // Email of users Array (ist eindeutig)
     "subTasks": ['Putzen','Spülen','Saubermachen'] 
   },
   {
     "id": 3,
     "title": "Kündigung der Mitarbeiterin XXX",
     "category": "Media",  // Design, Marketing, Sales, Backoffice, Media usw. gerne weitere Vorschläge bzw. Ergänzungen
+    "categorycolor": "#091931",
     "description": "Hier wird ein unnötiger Beschreibungstext stehen, der von dem User festgelegt wird",
     "dueDate": "30-10-2010",
     "priority": "Medium",       // Urgent, Medium, Low
     "status": "Awaiting feedback",     // To do, In progress, Awaiting feedback, Done
-    "assignedTo": [0,1,2],   // Index of users Array (ist eindeutig)
+    "assignedTo": ['ullrich.tobias@gmx.de','edip-bahcecioglu@hotmail.de','oswaldeugen95@gmail.com'],   // Email of users Array (ist eindeutig)
     "subTasks": ['Putzen','Spülen','Saubermachen'] 
   },
   {
     "id": 4,
     "title": "Einführung neues Softwaresystem ERBY",
     "category": "Marketing",  // Design, Marketing, Sales, Backoffice, Media usw. gerne weitere Vorschläge bzw. Ergänzungen
+    "categorycolor": "#091931",
     "description": "Hier wird ein unnötiger Beschreibungstext stehen, der von dem User festgelegt wird",
     "dueDate": "30-10-2010",
     "priority": "Medium",       // Urgent, Medium, Low
     "status": "Done",     // To do, In progress, Awaiting feedback, Done
-    "assignedTo": [0,1,2],   // Index of users Array (ist eindeutig)
+    "assignedTo": ['ullrich.tobias@gmx.de','edip-bahcecioglu@hotmail.de','oswaldeugen95@gmail.com'],   // Email of users Array (ist eindeutig)
     "subTasks": ['Putzen','Spülen','Saubermachen'] 
   },
   {
     "id": 5,
     "title": "Einführungsschulung betriebseigenes Canbanboard",
     "category": "Design",  // Design, Marketing, Sales, Backoffice, Media usw. gerne weitere Vorschläge bzw. Ergänzungen
+    "categorycolor": "#091931",
     "description": "Hier wird ein unnötiger Beschreibungstext stehen, der von dem User festgelegt wird",
     "dueDate": "30-10-2010",
     "priority": "Urgent",       // Urgent, Medium, Low
     "status": "Awaiting feedback",     // To do, In progress, Awaiting feedback, Done
-    "assignedTo": [0,1,2],   // Index of users Array (ist eindeutig)
+    "assignedTo": ['ullrich.tobias@gmx.de','edip-bahcecioglu@hotmail.de','oswaldeugen95@gmail.com'],   // Email of users Array (ist eindeutig)
     "subTasks": ['Putzen','Spülen','Saubermachen'] 
   },
   {
     "id": 6,
     "title": "Call popentcial clients",
     "category": "Sales",  // Design, Marketing, Sales, Backoffice, Media usw. gerne weitere Vorschläge bzw. Ergänzungen
+    "categorycolor": "#091931",
     "description": "Make the product presentation to prospective buyers",
     "dueDate": "05-08-2022",
     "priority": "Urgent",       // Urgent, Medium, Low
-    "status": "In progress",     // To do, In progress, Awaiting feedback, Done
-    "assignedTo": [0,1,2],   // Index of users Array (ist eindeutig)
+    "status": "In progress",    // To do, In progress, Awaiting feedback, Done
+    "assignedTo": ['ullrich.tobias@gmx.de','edip-bahcecioglu@hotmail.de','oswaldeugen95@gmail.com'],   // Email of users Array (ist eindeutig)
     "subTasks": ['Putzen','Spülen','Saubermachen'] 
   }
   ];
