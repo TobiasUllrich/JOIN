35ba86216740703b7a758ccdc385dcae430483b0
EdipRubenBahcecioglu <edip-bahcecioglu@hotmail.de>
2022-10-13 01:04:42 +0200

    edit task and move task added in backend included assigned users

--- js/board.js
+++ js/board.js
@@ -7,6 +7,7 @@ let currentDraggingElement;
 let editNewPrio;
 let assignedPeople = [];
 let selectedWorkers = [];
+let selectedWorkersEmail = [];
 let loggedUser = getArray('arrayOfactUser');
 
 /**
@@ -786,6 +787,9 @@ function submitChanges(idOfCurrentTask) {
     updateTaskArray(idOfCurrentTask, newTitle, newDescription, newDate, newPrio);
     setRequiredToDefault();
     editNewPrio = '';
+    selectedWorkers = [];
+    selectedWorkersEmail = [];
+    renderBoard();
 }
 
 /**
@@ -816,10 +820,12 @@ function submitCheckbox(idOfCheckbox) {
 function checkIfWorkerIsPushable(id) {
     if (!selectedWorkers.includes(users[id]) && id >= 0) {
         selectedWorkers.push(users[id]);
+        selectedWorkersEmail.push(users[id].email)
     }
     if (id == '-1') {
         if (!selectedWorkers.includes(loggedUser)) {
             selectedWorkers.push(loggedUser);
+            selectedWorkersEmail.push(loggedUser.email);
         }
     }
 }
@@ -834,9 +840,11 @@ function checkIfWorkerIsRemoveable(id) {
     for (let p = 0; p < selectedWorkers.length; p++) {
         if (selectedWorkers[p] === users[id]) {
             selectedWorkers.splice(p, 1);
+            selectedWorkersEmail.splice(p, 1);
         }
         if (selectedWorkers[p] == loggedUser) {
             selectedWorkers.splice(p, 1);
+            selectedWorkersEmail.splice(p, 1);
         }
     }
 }
@@ -909,7 +917,7 @@ function updateTaskArray(taskId, title, description, date, prio) {
     object['description']=description;
     object['dueDate']=date;
     object['priority']=prio;
-    // object['assignedTo']=['email1@web.de', email2, email3];
+    object['assignedTo']= selectedWorkersEmail;
     changeTask(object);
 
     // tasks[taskId].title = title;
